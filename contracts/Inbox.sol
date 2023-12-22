735b4c16f61feac092904e106c99de442cf469b7
abeselom-tsegazeab <a.abeselom.t@gmail.com>
2023-12-22 22:06:31 +0300

    inbox.sol

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -4,4 +4,9 @@ pragma solidity ^0.8.7;
 
 contract Inbox{
     string public welcome = "hello world";
+    string public greet = 'greeting';
+    function sayHi(string calldata _name) public pure  returns (string memory){
+        return _name; 
+
+    }
 }
