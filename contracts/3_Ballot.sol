e104200e8623e77e7fdb4c34d05daf7819638eb7
abeselom-tsegazeab <a.abeselom.t@gmail.com>
2024-01-31 21:14:27 +0300

    contract ballot

--- contracts/3_Ballot.sol
+++ contracts/3_Ballot.sol
@@ -8,12 +8,7 @@ pragma solidity >=0.7.0 <0.9.0;
  */
 contract Ballot {
 
-    struct Voter {
-        uint weight; // weight is accumulated by delegation
-        bool voted;  // if true, that person already voted
-        address delegate; // person delegated to
-        uint vote;   // index of the voted proposal
-    }
+   
 
     struct Proposal {
         // If you can limit the length to a certain number of bytes, 
